20f6665cca64030c82ba45329024f39053cf889b
Will Knez <wbknez.dev@gmail.com>
2017-10-16 16:31:22 -0600

    Add application entry point.

---
+++ telephone/main.py
@@ -0,0 +1,18 @@
+"""
+The main driver for Telephone, a simple simulation of an information network
+formed from telephone contact lists used to search for information.
+"""
+import sys
+
+
+def main():
+    """
+    The application entry point.
+
+    :return: An exit code.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
