eb091dbb700e602e977011c5660fdb2979489473
Will Knez <wbknez.dev@gmail.com>
2017-10-20 04:01:39 -0600

    Move class methods to outside functions.

--- telephone/network_gen.py
+++ telephone/network_gen.py
@@ -4,6 +4,33 @@
 import random
 
 
+def can_link_to(person, contact, model):
+    """
+
+    :param person: The person wanting to link.
+    :param contact: The contact to determine link eligibility.
+    :param model: The model to use.
+    """
+    if model.require_mutual and \
+            len(contact.contacts) >= contact.max_contacts and \
+            not person.unique_id in contact.contacts:
+        return False
+    return True
+
+
+def link_to(person, contact, model):
+    """
+
+    :param person:
+    :param contact:
+    :param model:
+    """
+    person.contacts.append(contact.unique_id)
+
+    if model.require_mutual:
+        contact.contacts.append(person.unique_id)
+
+
 class NetworkGenerator:
     """
 
@@ -12,13 +39,6 @@ class NetworkGenerator:
     def __init__(self, max_size):
         self.network = list(range(max_size))
 
-    def can_link_to(self, person, contact, model):
-        if model.require_mutual and \
-                        len(contact.contacts) >= contact.max_contacts and \
-                not person.unique_id in contact.contacts:
-            return False
-        return True
-
     def generate_for(self, person, model):
         """
 
@@ -26,21 +46,13 @@ class NetworkGenerator:
         :param model:
         """
         contacts = [x for x in self.network \
-                if not x == person.unique_id and not x in person.contacts]
-        in_theory = person.max_contacts - len(person.contacts)
-        num_contacts = min(in_theory, len(contacts))
+                    if not x == person.unique_id and not x in person.contacts]
 
         random.shuffle(contacts)
         for contact_id in contacts:
-            if len(person.contacts) >= num_contacts:
+            if len(person.contacts) >= person.max_contacts:
                 return
 
             contact = model.people[contact_id]
-            if self.can_link_to(person, contact, model):
-                self.link_to(person, contact, model)
-
-    def link_to(self, person, contact, model):
-        person.contacts.append(contact.unique_id)
-
-        if model.require_mutual:
-            contact.contacts.append(person.unique_id)
+            if can_link_to(person, contact, model):
+                link_to(person, contact, model)
