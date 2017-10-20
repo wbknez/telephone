290855172a218bfa2d6fc0c1e5ca13cab2b24ef1
Will Knez <wbknez.dev@gmail.com>
2017-10-20 02:08:07 -0600

    Update visualization parameters to be a dict with user-modifiable widgets.

--- telephone/server.py
+++ telephone/server.py
@@ -1,7 +1,9 @@
 """
 
 """
+import time
 from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.UserParam import UserSettableParameter
 from mesa.visualization.modules import CanvasGrid
 
 from .model import TelephoneModel
@@ -21,16 +23,29 @@ def person_portrayal(person):
     :param person: The person to visualize.
     :return: A new portrayal.
     """
-    return {"Shape": "circle", "Filled": "true", "r": "0.5", "Layer": "0",
+    return {"Shape": "circle" if not person.malicious else "rect",
+            "Filled": "true", "r": "0.5", "Layer": "0", "w": "0.5", "h": "0.5",
             "Color": "blue" if person.data else _STATE_COLORS[person.state]}
 
 
-grid = CanvasGrid(person_portrayal, 10, 10, 400, 400)
+_PARAMS = {
+    "seed": int(time.time()),
+    "num_people": UserSettableParameter("slider", "Number of Agents", 100, 2,
+                                        225, 1),
+    "data_prob": UserSettableParameter("slider", "Probability of Initial "
+                                                 "Knowledge",
+                                       0.05, 0.0, 1.0, 0.01),
+    "malicious_prob": UserSettableParameter("slider", "Probability of "
+                                                      "Maliciousness",
+                                            0.05, 0.0, 1.0, 0.01),
+    "search_prob": UserSettableParameter("slider", "Probability of Initial "
+                                                   "Search Desire",
+                                         0.05, 0.0, 1.0, 0.01),
+    "width": 15,
+    "height": 15
+}
+
+
+grid = CanvasGrid(person_portrayal, 15, 15, 500, 500)
 server = ModularServer(TelephoneModel, [grid],
-                       "Telephone Model", {"seed": 0,
-                                           "num_people": 100,
-                                           "malicious_prob": 0,
-                                           "data_prob": 0.05,
-                                           "search_prob": 0.05,
-                                           "width": 10,
-                                           "height": 10})
+                       "Telephone Model", _PARAMS)
