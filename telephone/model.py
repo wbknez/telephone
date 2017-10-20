bcad1325a04587454e9c78d01fbdabf36167581c
Will Knez <wbknez.dev@gmail.com>
2017-10-20 01:02:45 -0600

    Add simulation space (for visualization only) and initialization for individual agents.

--- telephone/model.py
+++ telephone/model.py
@@ -4,9 +4,57 @@
 from random import random
 
 from mesa import Model
+from mesa.space import SingleGrid
 from mesa.time import RandomActivation
 
 from .person import Person
+
+
+def is_malicious(model):
+    """
+    Returns whether or not a newly generated person should be considered
+    malicious or not by choosing a random number and comparing it to the
+    specified model's maliciousness probability.
+
+    :param model: The model to use.
+    :return: Whether or not a generated person is malicious.
+    """
+    return random() < model.params["malicious_prob"]
+
+
+def is_knowledgeable(model, malicious):
+    """
+    Returns whether or not a newly generated person already knows an arbitrary
+    bit of data by choosing a random number and comparing it to the specified
+    model's knowledge probability.
+
+    People that are malicious cannot also start the simulation with knowledge
+    about a bit of data, otherwise the simulation risks being stuck in an
+    infinite loop.
+
+    :param model: The model to use.
+    :param malicious: If the person is malicious.
+    :return: Whether or not a generated person already knows about a bit of
+    data.
+    """
+    return False if malicious else random() < model.params["data_prob"]
+
+
+def initial_state(model, data):
+    """
+    Returns the initial state a newly generated person should be in by
+    choosing a random number and comparing it to the specified model's search
+    probability.
+
+    People that already have knowledge of the data cannot be in a search state.
+
+    :param model: The model to use.
+    :param data: If the person already has data knowledge.
+    :return: The type of state a person should be in.
+    """
+    if data or model.params["search_prob"] <= random():
+        return Person.State.Waiting
+    return Person.State.Searching
 
 
 class TelephoneModel(Model):
@@ -16,20 +64,22 @@ class TelephoneModel(Model):
 
     def __init__(self, seed, **kwargs):
         super().__init__(seed)
-        self.data = {}
+        self.collector = None
+        self.data = {"waiting": 0, "reporting": 0, "searching": 0, "knowing": 0}
+        self.grid = SingleGrid(kwargs["width"], kwargs["height"], False)
         self.params = kwargs
         self.people = []
         self.schedule = RandomActivation(self)
 
         self.create_people()
         self.create_networks()
-        self.create_data_collectors()
+        self.create_data_collector()
 
     def __getattr__(self, item):
         if item == "steps":
             return self.schedule.steps
 
-    def create_data_collectors(self):
+    def create_data_collector(self):
         """
 
         """
@@ -45,15 +95,26 @@ class TelephoneModel(Model):
         """
 
         """
-        for i in range(self.params["num_people"]):
-            is_malicious = random() < self.params["malicious_prob"]
-            knows_data = random() < self.params["data_prob"]
-            start_state = Person.State.Searching \
-                if random() < self.params["search_prob"] and not knows_data \
-                else Person.State.Waiting
-            self.people.append(Person(i, self, data=knows_data,
-                                      malicious=is_malicious,
-                                      state=start_state))
+        current_id = 0
+
+        for w in range(self.params["width"]):
+            for h in range(self.params["height"]):
+                if current_id >= self.params["num_people"]:
+                    return
+                person = self.create_person(current_id)
+
+                self.grid.place_agent(person, (w, h))
+                self.people.append(person)
+                current_id += 1
+
+    def create_person(self, unique_id):
+        person = Person(unique_id, self)
+
+        person.malicious = is_malicious(self)
+        person.data = is_knowledgeable(self, person.malicious)
+        person.state = initial_state(self, person.data)
+
+        return person
 
     def step(self):
         """
