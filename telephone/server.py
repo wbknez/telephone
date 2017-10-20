a68284894507f995b83f57550a168a8f71348a36
Will Knez <wbknez.dev@gmail.com>
2017-10-20 00:29:30 -0600

    Update visualization to use a state map for colors.

--- telephone/server.py
+++ telephone/server.py
@@ -8,16 +8,21 @@ from .model import TelephoneModel
 from .person import Person
 
 
+_STATE_COLORS = {Person.State.Reporting: "green",
+                 Person.State.Searching: "red",
+                 Person.State.Waiting: "gray"}
+
+
 def person_portrayal(person):
     """
-
+    Returns the visualization attributes that describe the specified person's
+    current state and data knowledge.
 
     :param person: The person to visualize.
     :return: A new portrayal.
     """
     return {"Shape": "circle", "Filled": "true", "r": "0.5",
-            "Color": "blue" if person.data else "red" \
-                if person.state == Person.State.Searching else "gray"}
+            "Color": "blue" if person.data else _STATE_COLORS[person.state]}
 
 
 grid = CanvasGrid(person_portrayal, 10, 10, 400, 400)
